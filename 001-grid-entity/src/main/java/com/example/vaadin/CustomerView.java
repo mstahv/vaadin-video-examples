fe2d407793c0fbd77ef92a2c448a225532f83fc2
Drew Harvey <drew@vaadin.com>
2025-12-08 15:09:22 -0700

    001: splitting code into best and minimal examples, adding auto launch to app props

--- 001-grid-entity/src/main/java/com/example/vaadin/CustomerView.java
+++ 001-grid-entity/src/main/java/com/example/vaadin/CustomerView.java
@@ -15,30 +15,54 @@ import com.vaadin.flow.router.Route;
 public class CustomerView extends VerticalLayout {
 
   public CustomerView(CustomerRepository repository) {
+    bestPracticeExample(repository);
+    // minimalExample(repository);
+  }
+
+  /**
+   * Below is a more in-depth explanation of binding an entity to a {@link Grid} component.
+   * The code in this function represents a best-practice approach and is the preferred approach
+   * for production quality code.
+   * @param repository
+   */
+  private void bestPracticeExample(CustomerRepository repository) {
 
     // tell the view to take up all of the remaining screen space
     setSizeFull();
 
-    // create the grid component and generate columns for the Customer class
-    var grid = new Grid<Customer>(Customer.class);
+    // create the grid component and define our columns
+    var grid = new Grid<Customer>();
     grid.setSizeFull();
+    grid.addColumn(Customer::getId)
+      .setHeader("ID");
+    grid.addColumn(Customer::getName)
+      .setHeader("Name");
+    grid.addColumn(Customer::getEmail)
+      .setHeader("Email");
+
+    // automatically adjust column widths based on their content
+    grid.getColumns().forEach(c -> c.setAutoWidth(true));
+
+    // add the grid to our view
     add(grid);
 
     // fetch all of the customers and add them to our grid
     var customers = repository.findAll();
     grid.setItems(customers);
+  }
 
-    // end of demo --------------------------------
+  /**
+   * This is a minimal example used for focusing on the high-level concepts. Use this to 
+   * get an overview of the feature, but refer to the {@link #bestPracticeExample} 
+   * method for reference on production quality code.
+   */
+  private void minimalExample(CustomerRepository repository) {
 
-    // optional: set the desired order of columns
-    grid.setColumnOrder(
-      grid.getColumnByKey("id"),
-      grid.getColumnByKey("name"),
-      grid.getColumnByKey("email")
-    );
+    var grid = new Grid<Customer>(Customer.class);
+    add(grid);
 
-    // optional: automatically determine column widths based on content
-    grid.getColumns().forEach(c -> c.setAutoWidth(true));
+    var customers = repository.findAll();
+    grid.setItems(customers);
   }
 
 }
